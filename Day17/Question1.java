2c8a4e26e3ebf0629609f4a0b5242c3c28eafae9
AbhishekAgrawal71 <abhi713111@gmail.com>
2026-07-14 23:14:58 +0530

    Day 17 solution

---
+++ Day17/Question1.java
@@ -0,0 +1,41 @@
+package Day17;
+// Problem: Write a program to find the maximum and minimum values present in a given array of integers.
+
+// Input:
+// - First line: integer n
+// - Second line: n integers
+
+// Output:
+// - Print the maximum and minimum elements
+
+// Example:
+// Input:
+// 6
+// 3 5 1 9 2 8
+
+// Output:
+// Max: 9
+// Min: 1
+import java.util.*;
+public class Question1 {
+public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int arr[]=new int[n];
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+    int min=Integer.MAX_VALUE;
+    int max=Integer.MIN_VALUE;
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+        if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    System.out.print("The minimum and maximum elements are: "+min+" "+max);
+}
+}
