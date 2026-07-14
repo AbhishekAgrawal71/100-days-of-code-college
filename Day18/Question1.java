7ef7be88001af028f682f4d07d16c6f9fb850c1e
AbhishekAgrawal71 <abhi713111@gmail.com>
2026-07-14 23:41:14 +0530

    Day 18 solution

---
+++ Day18/Question1.java
@@ -0,0 +1,49 @@
+// Problem: Given an array of integers, rotate the array to the right by k positions.
+
+// Input:
+// - First line: integer n
+// - Second line: n integers
+// - Third line: integer k
+
+// Output:
+// - Print the rotated array
+
+// Example:
+// Input:
+// 5
+// 1 2 3 4 5
+// 2
+
+// Output:
+// 4 5 1 2 3
+import java.util.*;
+public class Question1 {
+public static void reverse_arr(int[] arr,int left,int right){
+    int temp;
+    while(left<right){
+        temp=arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
+        left++;
+        right--;
+    }
+}
+public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int arr[]=new int[n];
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+    int k=sc.nextInt();
+    k=k%n;
+    reverse_arr(arr,0,n-1);
+    reverse_arr(arr,0,k-1);
+    reverse_arr(arr,k,n-1);
+
+    for(int i=0;i<n;i++){
+        System.out.print(arr[i]+" ");
+    }
+}
+}
